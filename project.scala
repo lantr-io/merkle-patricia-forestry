981c176a355ada29bab7d1543ff907279b0fbef2
Alexander Nemish <anemish@gmail.com>
2025-02-16 14:56:29 +0100

    Scalus 0.8.5

--- project.scala
+++ project.scala
@@ -1,8 +1,8 @@
-//> using scala 3.3.4
+//> using scala 3.3.5
 //> using options -deprecation -feature
-//> using plugin org.scalus:scalus-plugin_3:0.8.4
-//> using dep org.scalus:scalus_3:0.8.4
-//> using dep org.scalus:scalus-bloxbean-cardano-client-lib_3:0.8.4
+//> using plugin org.scalus:scalus-plugin_3:0.8.5
+//> using dep org.scalus:scalus_3:0.8.5
+//> using dep org.scalus:scalus-bloxbean-cardano-client-lib_3:0.8.5
 //> using dep com.lihaoyi::upickle:4.1.0
 //> using dep org.slf4j:slf4j-simple:2.0.16
 //> using dep org.bouncycastle:bcprov-jdk18on:1.80
